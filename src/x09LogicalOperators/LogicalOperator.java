541b7238d2964004ff99dce8545acf324f56ce87
CJM <mrcmillington@gmail.com>
2020-09-01 06:26:52 +0100

    Selection 4/5 done

--- src/x09LogicalOperators/LogicalOperator.java
+++ src/x09LogicalOperators/LogicalOperator.java
@@ -2,6 +2,14 @@ package x09LogicalOperators;
 
 public class LogicalOperator {
     public static void main(String[] args) {
+        String Team = "AFC";
+        int place = 1;
+
+        if ( Team=="AFC" && place==1){
+            System.out.println("AND - Both are true");
+        }elif(Team=="AFC" || place==1){
+            System.out.println("OR - At least 1 is true");
+        }
 
     }
 
