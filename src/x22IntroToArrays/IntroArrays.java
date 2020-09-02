a42aba2be77c70cabb306c23883958ed5c7e4730
Chris Millington <millingtonc@lordwandsworth.org>
2020-09-02 12:56:49 +0100

    Arrays p1

--- src/x22IntroToArrays/IntroArrays.java
+++ src/x22IntroToArrays/IntroArrays.java
@@ -2,14 +2,21 @@ package x22IntroToArrays;
 
 public class IntroArrays {
     public static void main(String[] args) {
+        int[] ray = new int[5];
+        ray[0] = 19;
+        System.out.println(ray[0]);
+        int[] arry = {24,48,96,198};
+        System.out.println(arry[2]);
+        arry[3] = 76;
+        System.out.println(arry[2]);
 
+        extension();
     }
-
-
     /*
     Task
-
-
+        Create an Array of football teams below containing Chelsea,
+        Liverpool, AFC and Man City.
+        Then replace Man Utd with Man City
 
      */
 
@@ -18,10 +25,12 @@ public class IntroArrays {
 
 
 
-
-
-
-    private void extension(){
-
+    private static void extension(){
+    String teams[] = {"Chelsea","Liverpool","AFC","Man City"};
+    teams[3] = "Man Utd";
+    System.out.println(teams[0]);
+    System.out.println(teams[1]);
+    System.out.println(teams[2]);
+    System.out.println(teams[3]);
     }
 }
--- src/x23ArrayMethods/ArrayMethods.java
+++ src/x23ArrayMethods/ArrayMethods.java
@@ -1,13 +1,24 @@
 package x23ArrayMethods;
-
 public class ArrayMethods {
     public static void main(String[] args) {
-
+        int[] methods = {24,48,96,188};
+        System.out.println(methods.length);
+        for(int y = 0;y<methods.length;y++){
+            System.out.println(methods[y]);
+        }
+        for (int x:methods){
+            System.out.println(x);
+        }
+        extension();
     }
-
-
     /*
     Task
+        Loop through the List of teams from the last exercise.
+        Have the position number next to the team
+        e.g.    1.Chelsea,
+                2. Liverpool
+                3. .....
+
 
 
 
@@ -21,7 +32,10 @@ public class ArrayMethods {
 
 
 
-    private void extension(){
-
+    private static void extension(){
+        String teams[] = {"Chelsea","Liverpool","AFC","Man City"};
+        for(int y = 0;y<teams.length;y++){
+            System.out.printf("%2d. %s %n",y+1,teams[y]);
+        }
     }
 }
