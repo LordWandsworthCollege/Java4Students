23695fc38515103d6e50d35501e33a20e4e88aa6
mrcmillington <millingtonc@lordwandsworth.org>
2020-09-28 11:04:36 +0100

    Complete

---
+++ src/Variables/main.java
@@ -0,0 +1,14 @@
+// 1 - Hello World
+package Variables;
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World ");
+        aIntroToVariables x = new aIntroToVariables();
+        x.var();
+        bInput b = new bInput();
+
+        b.extension();
+    }
+}
+
+
--- src/x01Variables/HelloWorld.java
+++ src/x01Variables/HelloWorld.java
@@ -2,9 +2,7 @@ package x01Variables;
 
 public class HelloWorld {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+
 }
 
 
